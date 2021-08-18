eeb2336f6c8243c5fec08d744e9713d4c9e4f13a
Guillermo Orellana <wiyarmir@gmail.com>
2021-08-18 22:26:33 +0200

    add extensions_kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,52 +22,50 @@ subprojects {
         maven("https://jitpack.io")
         maven("https://kotlin.bintray.com/kotlinx")
     }
-
-    apply(plugin = "jacoco")
-    apply(plugin = "maven-publish")
-    apply(plugin = "kotlin")
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-
-    configure<JavaPluginExtension> {
-        withSourcesJar()
-        withJavadocJar()
-    }
-
-    configure<PublishingExtension> {
-        publications {
-            create<MavenPublication>("maven") {
-                from(components["java"])
-            }
-        }
-    }
-
-    afterEvaluate {
-
-        dependencies {
-            "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
-
-            "testImplementation"("org.assertj:assertj-core:3.20.2")
-            "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
-            "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
-            "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
-
-            "testImplementation"("org.jetbrains.kotlin:kotlin-test")
-            "testImplementation"("io.mockk:mockk:1.12.0")
-        }
-
-
-    }
-
-    val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-    val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-
-    compileKotlin.kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    compileTestKotlin.kotlinOptions {
-        jvmTarget = "1.8"
-    }
+//
+//    apply<JacocoPlugin>()
+//    apply<MavenPublishPlugin>()
+//    apply<KotlinMultiplatformPlugin>()
+//
+//    tasks.withType<Test> {
+//        useJUnitPlatform()
+//    }
+//
+//    configure<JavaPluginExtension> {
+//        withSourcesJar()
+//        withJavadocJar()
+//    }
+//
+//    configure<PublishingExtension> {
+//        publications {
+//            create<MavenPublication>("maven") {
+//                from(components["java"])
+//            }
+//        }
+//    }
+//
+//    afterEvaluate {
+//
+//        dependencies {
+//            "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
+//
+//            "testImplementation"("org.assertj:assertj-core:3.20.2")
+//            "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
+//            "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
+//            "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
+//
+//            "testImplementation"("org.jetbrains.kotlin:kotlin-test")
+//            "testImplementation"("io.mockk:mockk:1.12.0")
+//        }
+//    }
+//
+//    val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+//    val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+//
+//    compileKotlin.kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
+//    compileTestKotlin.kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
 }
--- extensions_kotlin/build.gradle.kts
+++ extensions_kotlin/build.gradle.kts
@@ -1,3 +1,33 @@
-dependencies {
-    "implementation"("com.github.komputing:khex:${Versions.khex}")
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.github.komputing:khex:${Versions.khex}")
+                implementation("com.ionspin.kotlin:bignum:0.3.1")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("org.assertj:assertj-core:3.20.2")
+                implementation("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
+                implementation("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
+                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
+
+                implementation("org.jetbrains.kotlin:kotlin-test")
+                implementation("io.mockk:mockk:1.12.0")
+            }
+        }
+    }
 }
