9b6e1d2705bc806ac67a86f48c2e9c8c4f62c34f
Guillermo Orellana <wiyarmir@gmail.com>
2021-08-18 23:01:01 +0200

    add keccak_shortcut

--- keccak_shortcut/build.gradle.kts
+++ keccak_shortcut/build.gradle.kts
@@ -1,4 +1,20 @@
-dependencies {
-    "implementation"("com.github.komputing:khex:${Versions.khex}")
-    "implementation"("com.github.komputing.khash:keccak:${Versions.khash}")
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.github.komputing:khex:${Versions.khex}")
+                implementation("com.github.komputing.khash:keccak:${Versions.khash}")
+                implementation("com.ionspin.kotlin:bignum:0.3.1")
+            }
+        }
+    }
 }
